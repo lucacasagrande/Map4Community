b24931ad70d65853f736f46b12e5015dbb0c14b0
Luca Delucchi <lucadeluge@gmail.com>
2013-09-23 15:21:33 +0200

    add website url to the user's information

--- add_user.php
+++ add_user.php
@@ -3,8 +3,9 @@ $name = htmlspecialchars(trim($_POST['name']));
 $city = htmlspecialchars(trim($_POST['company']));
 $lat = htmlspecialchars(trim($_POST['lat']));
 $lon = htmlspecialchars(trim($_POST['lon']));
+$url = htmlspecialchars(trim($_POST['website']));
 
-$line = array($lon,$lat,$name,$city);
+$line = array($lon,$lat,$name,$city,$url);
 
 $fp = fopen('user.csv', 'a');
 fputcsv($fp, $line, ";");
